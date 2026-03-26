9a2a8402a30241dfab6ad65df7838b552cf0388d
gonzalez-7 <20agonzalezp@gmail.com>
2026-03-25 23:21:06 -0700

    Created the ProjectsController

---
+++ COMP003B.Assignment2/Controllers/ProjectsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+	public class ProjectsController : Controller
+	{
+		public IActionResult Current()
+		{
+			ViewData["Title"] = "Current Projects";
+			return View();
+		}
+
+		public IActionResult Completed()
+		{
+			ViewData["Title"] = "Completed Projects";
+			return View();
+		}
+
+		public IActionResult Ideas()
+		{
+			ViewData["Title"] = "Project Ideas";
+			return View();
+		}
+	}
+}
